0571d20ae550a008f61dade45a503b1df0058173
dipcarbon <mkbo@me.com>
2020-05-08 22:38:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from identification import *
+from training import *
+import os
+
+weight_sample = """rpmc,0.999341229
+rpmj,0.931126682
+rpmh,0.65297138
+rpme,0.453734286
+rpmg,0.451085325
+rplx,0.414614517
+rpmd,0.337521184
+rpsp,0.328949972
+hupa,0.328484009
+hupb,0.327136496"""
